befd9aecb29b5d92a22a375a2f955db6e60a58c0
sanyapash <130094162+sanyapash@users.noreply.github.com>
2023-06-18 22:06:01 +0300

    one ball

---
+++ init.js
@@ -0,0 +1,9 @@
+
+window.onload = function()
+{
+    const initPerson = personGenerator.getPerson();
+    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('surnameOutput').innerText = initPerson.surname;
+    document.getElementById('genderOutput').innerText = initPerson.Gender;
+    document.getElementById('birthYearOutput').innerText = initPerson.birthYear;
+};
